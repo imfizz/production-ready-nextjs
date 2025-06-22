214b9195b14f4fb12f7e5a262626fe22e0fad044
Francis Albert Ilacad <75879858+imfizz@users.noreply.github.com>
2025-06-23 00:10:01 +0800

    chore: Add Tailwind CSS typography and animation packages, update Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,8 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  darkMode: ["class"],
+  darkMode: "class",
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -32,10 +27,25 @@ const config: Config = {
             },
             white: {
                 "100": "#F7F7F7",
+                DEFAULT: "#FFFFFF",
             },
-        }
+        },
+        fontFamily: {
+            "work-sans": ["var(--font-work-sans)"],
+        },
+        borderRadius: {
+            lg: "var(--radius)",
+            md: "calc(var(--radius) - 2px)",
+            sm: "calc(var(--radius) - 4px)",
+        },
+        boxShadow: {
+            100: "2px 2px 0px 0px rgb(0, 0, 0)",
+            200: "2px 2px 0px 2px rgb(0, 0, 0)",
+            300: "2px 2px 0px 2px rgb(238, 43, 105)",
+        },
+
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 }
-export default config 
+export default config;
