979c5d2863db8c35e71e6cacdb68f06ae8843a49
Francis Albert Ilacad <75879858+imfizz@users.noreply.github.com>
2025-06-23 23:20:26 +0800

    chore: Update Tailwind CSS configuration, replace deprecated packages, and add custom fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: "class",
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -10,42 +10,42 @@ const config: Config = {
   ],
   theme: {
     extend: {
-        screens: {
-            xs: "475px",
+      screens: {
+        xs: "475px",
+      },
+      colors: {
+        primary: {
+          "100": "#FFE8F0",
+          DEFAULT: "#EE2B69",
         },
-        colors: {
-            primary: {
-                "100": "#FFE8F0",
-                DEFAULT: "#EE2B69",
-            },
-            secondary: "#FBE843",
-            black: {
-                "100": "#333333",
-                "200": "#141413",
-                "300": "#7D8087",
-                DEFAULT: "#000000"
-            },
-            white: {
-                "100": "#F7F7F7",
-                DEFAULT: "#FFFFFF",
-            },
+        secondary: "#FBE843",
+        black: {
+          "100": "#333333",
+          "200": "#141413",
+          "300": "#7D8087",
+          DEFAULT: "#000000",
         },
-        fontFamily: {
-            "work-sans": ["var(--font-work-sans)"],
+        white: {
+          "100": "#F7F7F7",
+          DEFAULT: "#FFFFFF",
         },
-        borderRadius: {
-            lg: "var(--radius)",
-            md: "calc(var(--radius) - 2px)",
-            sm: "calc(var(--radius) - 4px)",
-        },
-        boxShadow: {
-            100: "2px 2px 0px 0px rgb(0, 0, 0)",
-            200: "2px 2px 0px 2px rgb(0, 0, 0)",
-            300: "2px 2px 0px 2px rgb(238, 43, 105)",
-        },
-
+      },
+      fontFamily: {
+        "work-sans": ["var(--font-work-sans)"],
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      boxShadow: {
+        100: "2px 2px 0px 0px rgb(0, 0, 0)",
+        200: "2px 2px 0px 2px rgb(0, 0, 0)",
+        300: "2px 2px 0px 2px rgb(238, 43, 105)",
+      },
     },
   },
   plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
-}
+};
+
 export default config;
