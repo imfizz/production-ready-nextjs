ae611639df90a3186c762735dda0d769db51f66a
Francis Albert Ilacad <75879858+imfizz@users.noreply.github.com>
2025-06-24 00:09:22 +0800

    feat: Integrate SearchForm component into home page and handle search parameters

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,11 @@
-export default function Home() {
+import SearchForm from "../components/SearchForm";
+
+export default async function Home({ searchParams }: {
+  searchParams: Promise<{ query?: string }>
+}) {
+
+  const query = (await searchParams).query;
+
   return (
     <>
       <section className="pink_container">
@@ -6,8 +13,8 @@ export default function Home() {
         <p className="sub-heading !max-w-3xl">
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
         </p>
+        <SearchForm query={query}/>
       </section>
-      <h1 className="heading">Home</h1>
     </>
   );
 }
