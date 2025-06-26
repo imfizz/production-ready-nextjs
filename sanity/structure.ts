de52660a9c168e60f98ce04f015588a76f501f45
Francis Albert Ilacad <75879858+imfizz@users.noreply.github.com>
2025-06-26 23:59:27 +0800

    feat: Update Sanity structure to include author document type and integrate author schema

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -4,4 +4,4 @@ import type {StructureResolver} from 'sanity/structure'
 export const structure: StructureResolver = (S) =>
   S.list()
     .title('Content')
-    .items(S.documentTypeListItems())
+    .items([S.documentTypeListItems('author').title("Authors")]);
