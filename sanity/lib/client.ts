d8ff26127ae5d7bedca7b19b70f5ea34bfbe34d4
Francis Albert Ilacad <75879858+imfizz@users.noreply.github.com>
2025-07-01 23:33:46 +0800

    feat: Enhance STARTUPS_QUERY to support search parameters and update data fetching in home page

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -4,6 +4,7 @@ import { apiVersion, dataset, projectId } from '../env'
 
 // useCdn true means that sanity will cache the content for 60s and then revalidate the content after every 60s.
 // if true it gets the data every refresh or live
+// for now, we make it false and we use "sanity live" to get the data
 export const client = createClient({
   projectId,
   dataset,
