99b9e65d9d93c096139a7128faf74fce4e2b4415
Francis Albert Ilacad <75879858+imfizz@users.noreply.github.com>
2025-07-09 17:14:40 +0800

    feat: Enhance Navbar with user avatar and icons, add UserStartups component for displaying author's startups

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,9 @@
 import { auth, signIn, signOut } from "@/auth";
+import { BadgePlus, LogOut } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Navbar = async () => {
     const session = await auth();
@@ -18,7 +19,8 @@ const Navbar = async () => {
                     {session && session?.user? (
                         <>
                             <Link href="/startup/create">
-                                <span>Create</span>
+                                <span className="max-sm:hidden">Create</span>
+                                <BadgePlus className="size-6 sm:hidden" />
                             </Link>
 
                             <form action={async () => {
@@ -27,12 +29,19 @@ const Navbar = async () => {
                                 await signOut({ redirectTo: "/" });
                             }}>
                                 <button type="submit">
-                                    Logout
+                                    <span className="max-sm:hidden">Logout</span>
+                                    <LogOut className="size-6 sm:hidden text-red-500" />
                                 </button>
                             </form>
 
                             <Link href={`/users/${session?.user?.id}`}>
-                                <span>{session?.user?.name}</span>
+                                <Avatar className="size-10">
+                                    <AvatarImage 
+                                        src={session?.user?.image || ''}
+                                        alt={session?.user?.name || ''}
+                                    />
+                                    <AvatarFallback>AV</AvatarFallback>
+                                </Avatar>
                             </Link>
                         </>
                     ): (
--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,9 +1,10 @@
-import { formatDate } from "@/lib/utils";
+import { cn, formatDate } from "@/lib/utils";
 import { EyeIcon } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { Author, Startup } from "@/sanity/types";
+import { Skeleton } from "./ui/skeleton";
 
 export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
 
@@ -39,7 +40,13 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
                     </Link>
                 </div>
                 <Link href={`/user/${author?._id}`}>
-                    <Image src="https://placehold.co/48x48" alt="placeholder" width={48} height={48} className="rounded-full" />
+                    <Image 
+                        src={author?.image!} 
+                        alt={author?.name!} 
+                        width={48} 
+                        height={48} 
+                        className="rounded-full" 
+                    />
                 </Link>
             </div>
 
@@ -63,5 +70,15 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
         </li>
     );
 };
+
+export const StartupCardSkeleton = () => (
+    <>
+        {[0, 1, 2, 3, 4].map((index: number) => (
+            <li key={cn('skeleton', index)}>
+                <Skeleton className="startups-card_skeleton"/>
+            </li>
+        ))}
+    </>
+);
 
 export default StartupCard;
