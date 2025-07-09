7d5a10e0b1897128157282334c68a4dd81d8c1cb
Francis Albert Ilacad <75879858+imfizz@users.noreply.github.com>
2025-07-09 22:25:33 +0800

    feat: Add playlist schema and integrate editor picks into startup detail page

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { client } from "@/sanity/lib/client";
-import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
+import { PLAYLIST_BY_SLUG_QUERY, STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
 import { formatDate } from "@/lib/utils";
 import { notFound } from "next/navigation";
 import React, { Suspense } from "react";
@@ -8,6 +8,7 @@ import Image from "next/image";
 import markdownit from "markdown-it";
 import { Skeleton } from "@/components/ui/skeleton";
 import View from "@/components/View";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 
 const md = markdownit();
 
@@ -17,6 +18,8 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
     const id = (await params).id;
 
     const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+
+    const { select: editorPosts } = await client.fetch(PLAYLIST_BY_SLUG_QUERY, { slug: 'editor-picker-new' });
 
     if(!post) return notFound();
 
@@ -67,7 +70,17 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
                 </div>
                 <hr className="divider" />
 
-                {/* TODO: EDITOR SELECTED STARTUPS */}
+                {editorPosts?.length > 0 && (
+                    <div className="max-w-4xl mx-auto">
+                        <p className="text-30-semibold">Editor Picks</p>
+                        <ul className="mt-7 card_grid-sm">
+                            {editorPosts.map((post: StartupTypeCard, i: number) => (
+                                <StartupCard key={i} post={post} />
+                            ))}
+                        </ul>
+                    </div>
+                )}
+
                 <Suspense fallback={<Skeleton className="view_skeleton"/>}>
                     <View id={id} />
                 </Suspense>
